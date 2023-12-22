d3372d7d3c7d97796b4d94f62afc424ea0e18235
Maja Celej <cmaja1825@gmail.com>
2023-12-22 09:29:55 +0100

    formated code

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,10 @@
-import "./style.css"
+import "./style.css";
 
 const Form = () => (
     <form className="form">
-              <input className="form__newTask" placeholder="Co jest do zrobienia?" name="To do" type="text"
-                  autocomplete="off"/>
-              <button className="form__button">Dodaj zadanie</button>
-          </form>
+     <input className="form__newTask" placeholder="Co jest do zrobienia?" name="To do" type="text" autocomplete="off"/>
+     <button className="form__button">Dodaj zadanie</button>
+    </form>
 );
 
 export default Form;
